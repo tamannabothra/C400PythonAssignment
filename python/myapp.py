bbc0988649fcd95492c4c91d27cbee7d6c798b00
Tamanna Bothra <89736338+tamannabothra@users.noreply.github.com>
2024-10-04 14:13:34 +0530

    myapp.py

--- python/myapp.py
+++ python/myapp.py
@@ -2,7 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def hello_world():
-    return 'Welcome, I am Tamanna Bothra'
+    return 'Welcome to jenkins, I am Tamanna Bothra'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
