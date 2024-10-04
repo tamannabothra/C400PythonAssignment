75538181552a010159bebf651984bd903ed34922
Tamanna Bothra <89736338+tamannabothra@users.noreply.github.com>
2024-10-04 14:32:21 +0530

    Update myapp.py

--- python/myapp.py
+++ python/myapp.py
@@ -2,7 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def hello_world():
-    return 'I am Tamanna Bothra'
+    return 'I am Tamanna Bothra,welcome'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
